592e69c7f5aea59c7f6e94ae2e04d98a369f1fac
daurnimator <quae@daurnimator.com>
2018-03-25 22:06:26 +1100

    test/analyse_requires.test.js: Add tests for analyse_requires

---
+++ test/analyse_requires.test.js
@@ -0,0 +1,33 @@
+import { analyse_requires } from '../src/analyse_requires.js';
+
+describe('Analysis of lua require calls', () => {
+	test('basic call', () => {
+		expect(analyse_requires(`require "foo"`)).toEqual(['foo']);
+	});
+	test('with dotted module', () => {
+		expect(analyse_requires(`require "foo.bar"`)).toEqual(['foo.bar']);
+	});
+	test('with global assignment', () => {
+		expect(analyse_requires(`foo = require "foo"`)).toEqual(['foo']);
+	});
+	test('with local assignment', () => {
+		expect(analyse_requires(`local foo = require "foo"`)).toEqual(['foo']);
+	});
+	test('with conditional require', () => {
+		expect(analyse_requires(`if somecond() then require "foo" end`)).toEqual(['foo']);
+	});
+	test('with tail call', () => {
+		expect(analyse_requires(`return require "foo"`)).toEqual(['foo']);
+	});
+	test('with many requires', () => {
+		expect(analyse_requires(`
+local foo = require "foo"
+local bar = require "foo.bar"
+require "static"
+function more()
+	require "morethings"
+end
+return require "otherthing"
+		`).sort()).toEqual(['foo', 'foo.bar', 'static', 'morethings', 'otherthing'].sort());
+	});
+});
